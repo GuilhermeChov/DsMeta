da83fc802ff3259d8c022627429c52bd99d87175
Guilherme Ferreira <ferreiraguilherme2501@gmail.com>
2022-07-12 14:32:42 -0300

    Criação Projeto

---
+++ backend/src/test/java/com/chov/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chov.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
